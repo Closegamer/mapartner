41b04760dd129dc2b47791e82d7a0e1dfe68f61e
Alexander Grigoriev <avgrigorev@hse.ru>
2023-11-01 03:16:11 +0300

    no roistat counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,18 +46,6 @@ export default async function RootLayout({
             });`
                 }
             </Script>
-            {/* Roistat Counter Start */}
-
-            <Script id="roistat-counter" strategy="afterInteractive">
-            {`(function(w, d, s, h, id) {
-    w.roistatProjectId = id; w.roistatHost = h;
-    var p = d.location.protocol == "https:" ? "https://" : "http://";
-    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
-    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-})(window, document, 'script', 'cloud.roistat.com', 'cf9a2e328df66c8900b7f6ac4210d4e3');`
-            }
-            </Script>
-            {/* Roistat Counter End */}
           {children}
           <Footer />
           <ScrollToTop />
