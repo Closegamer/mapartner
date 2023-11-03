d5f092e56003bce71f9fb3c7ff3dfdc7f974de88
Alexander Grigoriev <avgrigorev@hse.ru>
2023-11-03 16:07:57 +0300

    admittad verif

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,7 @@ export default function Head() {
         <meta name="description" content="Сервис предоставления выбора финансовых решений" />
         <meta name="keywords" content="деньги, быстро, одолжить, займ, заём, кредит, плохая, кредитная, история" />
         <meta name="yandex-verification" content="7c67e02da54ee87c" />
+        <meta name="verify-admitad" content="a77319cf82" />
         <meta property="og:title" content="Сервис MoneyAc"/>
         <meta property="og:description" content="Сервис предоставления выбора финансовых решений"/>
         <meta property="og:image" content="https://www.moneyac.ru/logo_black.png"/>
