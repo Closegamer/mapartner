f72038c76a6008cdefa4b8ccedb96abed7990eeb
Alexander Grigoriev <avgrigorev@hse.ru>
2023-11-05 23:57:17 +0300

    GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import Script from 'next/script';
 import React from 'react';
 require('dotenv').config()
 import { YandexMetrika } from '../components/YandexMetrika';
+const GTM_ID = 'GTM-XXXXXXX';
 
 export default async function RootLayout({
   children
@@ -52,6 +53,15 @@ export default async function RootLayout({
                   webvisor:true
             });`
           }
+        </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','G-8HK5DFE920');
+          `}
         </Script>
         <Providers>
           <YandexMetrika />
