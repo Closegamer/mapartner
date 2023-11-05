088979323099702c382bf8be63aaab70be3076c5
Alexander Grigoriev <avgrigorev@hse.ru>
2023-11-05 20:37:49 +0300

    metrika moved

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,24 +36,24 @@ export default async function RootLayout({
       <head />
 
       <body className="dark:bg-black">
+          <Script id="metrika-counter" strategy="afterInteractive">
+              {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+     
+                ym(95430506, "init", {
+                      defer: true,
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                });`
+              }
+          </Script>
         <Providers>
           <Header session={session} admin={admin} />
-            <Script id="metrika-counter" strategy="afterInteractive">
-                {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-            ym(95430506, "init", {
-                  defer: true,
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true,
-                  webvisor:true
-            });`
-                }
-            </Script>
           {children}
           <Footer />
           <ScrollToTop />
