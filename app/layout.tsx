ff3f79f20e1c0db945c8fbedb048e03dd1bf5137
Alexander Grigoriev <avgrigorev@hse.ru>
2023-11-05 21:04:54 +0300

    ym new place

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,9 @@ export default async function RootLayout({
       <head />
 
       <body className="dark:bg-black">
-          <Script id="metrika-counter" strategy="afterInteractive">
-              {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        <Providers>
+            <Script id="metrika-counter" strategy="afterInteractive">
+                {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();
                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -50,9 +51,8 @@ export default async function RootLayout({
                       accurateTrackBounce:true,
                       webvisor:true
                 });`
-              }
-          </Script>
-        <Providers>
+                }
+            </Script>
           <Header session={session} admin={admin} />
           {children}
           <Footer />
