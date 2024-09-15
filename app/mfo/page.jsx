80a59e42e0087f5b8692ff5c75ec707c4a79ced9
Alexander Grigoriev <closegamer@mail.ru>
2024-09-15 13:18:34 +0300

    bot+

--- app/mfo/page.jsx
+++ app/mfo/page.jsx
@@ -16,15 +16,22 @@ const MFOPage = () => {
                 description="Россия и Казахстан"
             />
 
-            <Mfofive />
-            <Mfotwo />
-            <Mfoone />
-            <Mfothree />
-            <Mfofour />
-            <hr />
-            <MfoCZone />
-            <MfoCZtwo />
-            <MfoCZthree />
+            <Mfofive/>
+            <Mfotwo/>
+            <Mfoone/>
+            <Mfothree/>
+            <Mfofour/>
+            <hr/>
+            <div className={'text-center'}>
+                <p className="mb-2 mt-10 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">Больше возможностей предоставляет наш бесплатный телеграм-бот:</p>
+                <p className={'mb-10 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl'}>
+                    <a className={'text-blue-500 animate-pulse'} href={'https://t.me/MoneyAcBot_bot'}>@MoneyAcBot_bot</a>
+                </p>
+            </div>
+            <hr/>
+            <MfoCZone/>
+            <MfoCZtwo/>
+            <MfoCZthree/>
         </>
     );
 };
